d22f7143a36eaab8489a6eeb64cddfb431212679
MJ-MacBook <pmz671712@gmail.com>
2019-07-04 13:46:20 +0900

    0703 Ver.1

---
+++ Team Project/main_arduino/main_arduino.ino
@@ -0,0 +1,56 @@
+/*
+ * Main Arduino
+ * Version 20190703
+ * Read sound data every 0.1 sec
+ * If sound is larger than LOUD_SOUND,
+ * buzzer_melody, led_blink
+ */
+
+#include "pitches.h"
+ 
+const int LOUD_SOUND = 650;
+const int mySoundSensor = A0;
+const int myBuzzer = 9;
+const int subArduino = 7;
+
+void setup() {
+  putMode(mySoundSensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int readSound = analogRead(A0);
+
+  if(readSound > LOUD_SOUND)
+  {
+    digitalWrite(subArduino,HIGH);
+    led_blink();
+    buzzer_melody();
+  }
+}
+
+void led_blink()
+{
+  
+}
+
+void buzzer_melody()
+{
+    int melody[] = {
+    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  };
+
+    int noteDurations[] = {
+    4, 8, 8, 4, 4, 4, 4, 4
+  };
+
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(myBuzzer, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(myBuzzer);
+  }
+}
---
+++ Team Project/sub_arduino/sub_arduino/sub_arduino.ino
@@ -0,0 +1,21 @@
+/*
+ * Sub Arduino
+ * Version 20190703
+ * If mainArduino pin is HIGH,
+ * start motor control
+ */
+
+const int mainArduino = 7;
+int myMotor = 9;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+void motor_control() {
+  
+}
